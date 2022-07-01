84fa4eee780c8e978bbca5f034909a4d11d1ce27
Ali Moradzade <alimorizz1379@gmail.com>
2022-07-02 00:32:44 +0430

    changing some logic in knn, handling divide by zero in embeding code! (i don't know why TA code has bug!)

--- phase03/embeding.py
+++ phase03/embeding.py
@@ -5,10 +5,9 @@ import numpy as np
 from pathlib import Path
 
 
-
 class DocToVec:
-    
-    def __init__(self, dataset:List[List] , vec_size = 200 , model_path = 'word2vec.model'):
+
+    def __init__(self, dataset: List[List], vec_size=200, model_path='word2vec.model'):
         """ Here we train our model and calculate tf-idf weights.
 
         Args:
@@ -19,47 +18,44 @@ class DocToVec:
         """
 
         self.vec_size = vec_size
-        
-        
-        
+
         if not Path(model_path).is_file():
             print('There is no pre-trained model. Going to train a model ...')
             self.wordToVecModel = Word2Vec(
-                window = 10,
+                window=10,
                 min_count=2,
                 workers=4,
-                vector_size = self.vec_size
+                vector_size=self.vec_size
             )
-        
+
             self.wordToVecModel.build_vocab(dataset)
-            
+
             self.wordToVecModel.train(
                 dataset,
-                total_examples = self.wordToVecModel.corpus_count,
-                epochs = 20,
+                total_examples=self.wordToVecModel.corpus_count,
+                epochs=20,
             )
-            
+
             self.wordToVecModel.save(model_path)
-            
+
         else:
-            
+
             print('Loading the model ...')
             self.wordToVecModel = Word2Vec.load(model_path)
-        
+
         con_train_data = [" ".join(a) for a in dataset]
-        
-        self.tfIdfVectorizer=TfidfVectorizer(use_idf=True,
-                                        dtype = np.float64,
-                                        lowercase = False,
-                                        vocabulary = self.wordToVecModel.wv.index_to_key)
-        
+
+        self.tfIdfVectorizer = TfidfVectorizer(use_idf=True,
+                                               dtype=np.float64,
+                                               lowercase=False,
+                                               vocabulary=self.wordToVecModel.wv.index_to_key)
+
         self.tfIdfVectorizer.fit(con_train_data)
-        
+
         dictionary = self.tfIdfVectorizer.get_feature_names()
-        self.dictToNum = {d:i for i,d in enumerate(dictionary)}
-        
-        
-    def embed(self, tokens:List):
+        self.dictToNum = {d: i for i, d in enumerate(dictionary)}
+
+    def embed(self, tokens: List):
         """Maps the input to a vector
 
         Args:
@@ -69,11 +65,11 @@ class DocToVec:
         Returns:
             Numpy Array: The vector of document
         """
-        
+
         ti = self.tfIdfVectorizer.transform([" ".join(tokens)])
-        
+
         weights = np.squeeze(np.asarray(ti[0].T.todense()))
-        
+
         vec = np.zeros((self.vec_size,))
         sum_weights = 0
         for k in set(tokens):
@@ -84,6 +80,7 @@ class DocToVec:
                 sum_weights += weight
             except KeyError:
                 pass
-        vec /= sum_weights
+
+        if sum_weights != 0:
+            vec /= sum_weights
         return vec
-        
